efcaab0ff082a63eb493e1824842878eef7ed3a6
Ciyong Chen <ciyong.chen@intel.com>
2017-05-02 16:57:00 +0800

    add test cases for group conv

--- tests/keras/layers/test_convolutional.py
+++ tests/keras/layers/test_convolutional.py
@@ -135,6 +135,31 @@ def test_convolution_2d():
 
 
 @keras_test
+def test_group_convolution_2d():
+    nb_samples = 2
+    nb_filter = 2
+    stack_size = 3
+    nb_row = 10
+    nb_col = 6
+
+    for border_mode in _convolution_border_modes:
+        for subsample in [(1, 1), (2, 2)]:
+            if border_mode == 'same' and subsample != (1, 1):
+                continue
+
+            for group in [1, 2]:
+                layer_test(convolutional.Convolution2D,
+                           kwargs={'nb_filter': nb_filter,
+                                   'nb_row': 3,
+                                   'nb_col': 3,
+                                   'border_mode': border_mode,
+                                   'subsample': subsample,
+                                   'group': group},
+                           input_shape=(nb_samples, nb_row, nb_col, stack_size),
+                           fixed_batch_size=True)
+
+
+@keras_test
 def test_deconvolution_2d():
     nb_samples = 2
     nb_filter = 2
